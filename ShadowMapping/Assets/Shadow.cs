8718742f7a7efdf613edab8a8cff990cfba0c59d
Macx <Macx@drkdeMacBook-Pro.local>
2016-06-17 15:21:30 +0800

    a project of shadowmapping use the depth buf to render to a texture and cast the texture to the ground

---
+++ ShadowMapping/Assets/Shadow.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Shadow : MonoBehaviour {
+
+	public RenderTexture t;	
+	
+	void Update () {
+		renderer.material.mainTexture = t;
+	}
+	
+	void OnGUI()
+	{
+		GUI.DrawTexture(new Rect(0,0,100,100),t);
+	}
+}
